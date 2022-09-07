340bcdbd7d6e03e6e2bec671c6ab59de898751f3
davigon <dvgnlz@outlook.es>
2022-09-07 19:45:56 +0200

    feat(client): not found page

--- src/client/pages/NotFoundPage.tsx
+++ src/client/pages/NotFoundPage.tsx
@@ -1,9 +1,28 @@
 import React from "react"
+import {
+  Box,
+  useColorModeValue,
+  Flex,
+  Text,
+  Heading,
+  Icon,
+  VStack,
+  HStack,
+} from "@chakra-ui/react"
+import { TbError404 } from "react-icons/tb"
 
 export const NotFoundPage = () => {
   return (
-    <div className="NotFound">
-      <>Not found!</>
-    </div>
+    <Box bg={useColorModeValue("gray.50", "gray.900")}>
+      <Flex h={"calc(100vh - 8rem)"} align={"center"} justify={"center"}>
+        <VStack>
+          <HStack>
+            <Heading color={"red.600"}>Error</Heading>
+            <Icon as={TbError404} boxSize={16} color={"red.600"} />
+          </HStack>
+          <Text fontSize={"xl"}>La página que buscas no existe.</Text>
+        </VStack>
+      </Flex>
+    </Box>
   )
 }
